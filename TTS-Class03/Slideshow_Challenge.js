7f1f38133eb126105a48baedb848c89b90394e5a
Christopher Elibert <eligoodfella@gmail.com>
2021-01-21 17:23:20 -0500

    Final Version

--- TTS-Class03/Slideshow_Challenge.js
+++ TTS-Class03/Slideshow_Challenge.js
@@ -1,30 +1,25 @@
 let slideshow = {
-    self: this,
-    photolist: ['name1', 'name2', 'name3'],
-    currentPhotoIndex: 0,
+    photolist: ['name1', 'name2'],
+    currentPhotoIndex: 2,
     nextPhoto: function(){
-        if(this.currentPhotoIndex <= this.photolist.length){
+        if(this.currentPhotoIndex){
             this.currentPhotoIndex += 1;
-            //console.log(this.photoList[this.currentPhotoIndex]);
-            //
-            self.getCurrentPhoto();
+            console.log(this.photoList[this.currentPhotoIndex]);
         }
         else {
-           // console.log("End of slideshow");
+            console.log("End of slideshow");
         }
     },
     prevPhoto: function(){
-        if(this.currentPhotoIndex > 0){
+        if(this.currentPhotoIndex){
             this.currentPhotoIndex -= 1;
-            // console.log(this.photoList[this.currentPhotoIndex]);
+            console.log(this.photoList[this.currentPhotoIndex]);
         }
         else {
-            // console.log("End of slideshow");
+            console.log("End of slideshow");
         }
     },
     getCurrentPhoto: function(){
-        return this.photoList[this.currentPhotoIndex];
-     console.log(slideshow.getCurrentPhoto());
-     console.log(slideshow.nextPhoto());
+        return this.currentPhotoIndex;
     } 
 }
